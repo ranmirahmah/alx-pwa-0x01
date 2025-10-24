d7903e92e66a47ea08234b850262c749f693039f
ranmirahmah <ranmigold01@gmail.com>
2025-10-24 15:33:05 +0100

    build

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,16 @@
-import type { NextConfig } from "next";
+import withPWA from '@ducanh2912/next-pwa';
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactStrictMode: true,
 };
 
-export default nextConfig;
+export default withPWA({
+  dest: 'public',
+  register: true,
+  disable: process.env.NODE_ENV === 'development',
+  workboxOptions: {
+    skipWaiting: true,
+    clientsClaim: true,
+  },
+})(nextConfig);
